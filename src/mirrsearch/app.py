84dc2c8f678cd11a58dd5b5ae9684e2135eac37c
Christine Colvin <christinegracecolvin@gmail.com>
2026-02-24 16:44:06 -0500

    changed internallogic.py and app.py to include new agency parameters for filtering

--- src/mirrsearch/app.py
+++ src/mirrsearch/app.py
@@ -17,13 +17,14 @@ def create_app():
     @flask_app.route("/search/")
     def search():
         search_input = request.args.get('str')
-        filter_param = request.args.get('filter')  # e.g. /search/?str=renal&filter=Proposed Rule
+        filter_param = request.args.get('filter') # e.g. /search/?str=renal&filter=Proposed Rule
+        agency_param = request.args.get('agency') 
 
         if search_input is None:
             search_input = "example_query"
 
         logic = InternalLogic("sample_database")
-        return jsonify(logic.search(search_input, filter_param))
+        return jsonify(logic.search(search_input, filter_param, agency_param))
 
     return flask_app
 
