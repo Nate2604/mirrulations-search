ed4e44677f6ddc98e71402248c5174c4f1abde5d
Rafael Garcia Jr. <Rgarcia.dx@gmail.com>
2026-02-24 20:42:57 -0500

    Update app.py, internal_logic.py to inject mock in tests

--- src/mirrsearch/app.py
+++ src/mirrsearch/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from mirrsearch.internal_logic import InternalLogic
 
 
-def create_app():
+def create_app(db_layer=None):
     # This is needed due to templates being 2 levels up from this file causing flask not to see it.
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
     static_dir = os.path.join(project_root, 'static')
@@ -17,13 +17,11 @@ def create_app():
     @flask_app.route("/search/")
     def search():
         search_input = request.args.get('str')
-        filter_param = request.args.get('filter') # e.g. /search/?str=renal&filter=Proposed Rule
+        filter_param = request.args.get('filter')
         agency_param = request.args.get('agency')
-
         if search_input is None:
             search_input = "example_query"
-
-        logic = InternalLogic("sample_database")
+        logic = InternalLogic("sample_database", db_layer=db_layer)
         return jsonify(logic.search(search_input, filter_param, agency_param))
 
     return flask_app
@@ -32,3 +30,4 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(port=80, debug=True)
+    
