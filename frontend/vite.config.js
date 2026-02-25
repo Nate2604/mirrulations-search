155d76224f49e6a68130805c2591b8ebe330737f
Cole Aydelotte <aydelottec@moravian.edu>
2026-02-24 20:04:22 -0500

    Fixed dev scripting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/static/',
-    build: {
-        outDir: '../static',
-        emptyOutDir: false
-    },
     server: {
         proxy: {
             '/search': 'http://127.0.0.1:80'
