d6d5ad230bd3ad3cd6a6ef62656efd5abd44b842
Jalil <rodriguezj07@moravian.edu>
2026-02-27 16:36:24 -0500

    Added a url to the cfr part that brings you to the Regulations.gov website

--- frontend/src/components/ResultsPanel.jsx
+++ frontend/src/components/ResultsPanel.jsx
@@ -1,3 +1,5 @@
+const CFR_BASE_URL = "https://www.ecfr.gov/search#query";
+
 export default function ResultsPanel({ results }) {
   if (!results || results.length === 0) {
     return (
@@ -11,15 +13,13 @@ export default function ResultsPanel({ results }) {
     <div className="results">
       {results.map((item, index) => (
         <div key={item.docket_id || index} className="result-card">
-          
-          <h3 className="result-title">
-            {item.title}
+          <h3 className="result-title">{item.title}
           </h3>
 
           <div className="result-meta">
             <p><strong>Agency:</strong> {item.agency_id}</p>
             <p><strong>Document Type:</strong> {item.document_type}</p>
-            <p><strong>CFR:</strong> {item.cfrPart}</p>
+            <a href={CFR_BASE_URL}><p><strong>CFR:</strong> {item.cfrPart}</p></a>
             <p><strong>Publication Date:</strong> {item.publication_date}</p>
           </div>
 
